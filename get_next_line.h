9c86d049f2e6ddd2740421ebe0b5155396e37c26
DiegoR <128084117+The-World-of-DD@users.noreply.github.com>
2025-03-23 21:31:33 +0100

    hhh

--- get_next_line.h
+++ get_next_line.h
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dierojas <dierojas@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: dierojas < dierojas@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 21:33:29 by dierojas          #+#    #+#             */
-/*   Updated: 2025/03/08 15:17:26 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/03/23 21:31:30 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+# endif
 
 #include <stdlib.h>
 #include <unistd.h>
